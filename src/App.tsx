82ad509cd96e1fb7d7d1cf59f83365eb7a1c8dda
Ирина Елисеева <irina.eliseeva1991@gmail.com>
2023-12-27 22:47:56 +0300

    sprint02 wee01 useEffect was added

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,30 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import styled from "styled-components";
 
 function App() {
-  const [value, setValue] = useState(0)
-  const incHandler = () => {
-    setValue(value + 1)
-  };
-  const setToLocalStorageHandler = () => {
-    localStorage.setItem('counterValue', JSON.stringify(value));
-    localStorage.setItem('counterValue + 1', JSON.stringify(value + 1));
-  };
-  const getFromLocalStorageHandler = () => {
+  const [value, setValue] = useState(0);
+
+  useEffect( () => {
     let valueAsString = localStorage.getItem('counterValue');
     if (valueAsString) {
       let newValue = JSON.parse(valueAsString);
-      setValue(newValue)
+      setValue(newValue);
     }
+  }, []);
+
+  useEffect( () => {
+    localStorage.setItem('counterValue', JSON.stringify(value));
+  }, [value])
+  const incHandler = () => {
+    setValue(value + 1)
   };
-  const clearLocalStorageHandler = () => {
-    localStorage.clear();
-    setValue(0)
-  };
-  const removeItemFromLocalStorageHandler = () => {
-    localStorage.removeItem('counterValue + 1')
-  }
 
   return (
     <AppWrapper>
       <p>Counter</p>
       {value}
       <button onClick={incHandler}>inc</button>
-      <button onClick={setToLocalStorageHandler}>setToLocalStorage</button>
-      <button onClick={getFromLocalStorageHandler}>getFromLocalStorage</button>
-      <button onClick={clearLocalStorageHandler}>clearLocalStorage</button>
-      <button onClick={removeItemFromLocalStorageHandler}>removeItemFromLocalStorage</button>
-
     </AppWrapper>
   );
 }
