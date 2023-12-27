d8acbf37b886ef8e6dbd3087563e6abf70316ca2
Ирина Елисеева <irina.eliseeva1991@gmail.com>
2023-12-27 22:24:15 +0300

    sprint02 week01 all function of local storage were added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import styled from "styled-components";
 
 function App() {
+  const [value, setValue] = useState(0)
+  const incHandler = () => {
+    setValue(value + 1)
+  };
+  const setToLocalStorageHandler = () => {
+    localStorage.setItem('counterValue', JSON.stringify(value));
+    localStorage.setItem('counterValue + 1', JSON.stringify(value + 1));
+  };
+  const getFromLocalStorageHandler = () => {
+    let valueAsString = localStorage.getItem('counterValue');
+    if (valueAsString) {
+      let newValue = JSON.parse(valueAsString);
+      setValue(newValue)
+    }
+  };
+  const clearLocalStorageHandler = () => {
+    localStorage.clear();
+    setValue(0)
+  };
+  const removeItemFromLocalStorageHandler = () => {
+    localStorage.removeItem('counterValue + 1')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrapper>
+      <p>Counter</p>
+      {value}
+      <button onClick={incHandler}>inc</button>
+      <button onClick={setToLocalStorageHandler}>setToLocalStorage</button>
+      <button onClick={getFromLocalStorageHandler}>getFromLocalStorage</button>
+      <button onClick={clearLocalStorageHandler}>clearLocalStorage</button>
+      <button onClick={removeItemFromLocalStorageHandler}>removeItemFromLocalStorage</button>
+
+    </AppWrapper>
   );
 }
 
 export default App;
+
+const AppWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+`
+
+
